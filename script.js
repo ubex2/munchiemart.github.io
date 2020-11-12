ce3d71ebc2b722fd18725f8c9feb65f63c3f0828
ubex2 <59666795+ubex2@users.noreply.github.com>
2020-11-11 20:50:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function displayCookies() {
     e_cookieQuantity.innerHTML = Math.floor(cookies).toLocaleString();
 
     // Update the title to match
-    document.title = Math.floor(cookies).toLocaleString() + "Munchie Mart | " + " " + e_cookieQuantity_under.innerHTML ;
+    document.title = Math.floor(cookies).toLocaleString() + "Munchie Mart | " + " " + e_cookieQuantity_under.innerHTML;
 }
 
 // Adds items to the store
